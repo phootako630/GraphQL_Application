335c405ece9bb07fb72ac1818ebceb98d18747c1
ivanzhang <ivanzhang220820@gmail>
2022-11-28 00:27:51 -0500

    add mutation type: delete, update and create

--- data/dbConnector.js
+++ data/dbConnector.js
@@ -1,4 +1,8 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import { Sequelize, DataTypes} from "sequelize";
+import _ from 'lodash';
+import casual from "casual";
+
 
 //connection
 mongoose.Promise = global.Promise;
@@ -30,4 +34,19 @@ const widgetSchema = new mongoose.Schema({
 
 const Widgets = mongoose.model('widgets', widgetSchema);
 
-export {Widgets}
+const sequelize = new Sequelize('sqlite::memory');
+
+const Categories = sequelize.define('categories', {
+    category: DataTypes.STRING,
+    description: DataTypes.STRING,
+})
+Categories.sync({ force: true } ).then(() => {
+    _.times(5, (i) => {
+        Categories.create({
+            category: casual.word,
+            description: casual.sentence,
+        })
+    })
+});
+
+export { Widgets, Categories }
--- data/schema.js
+++ data/schema.js
@@ -1,44 +1,49 @@
-import  { buildSchema } from "graphql";
+import { buildSchema } from "graphql";
 
 const schema = buildSchema(`
-        type Product {
-            id: ID
-            name: String
-            description: String
-            price: Float
-            soldout: Soldout
-            inventory: Int
-            stores: [Store]!
-        }
-        enum Soldout {
-          SOLDOUT
-          ONSALE
-        }
-        
-        type Store {
+    type Product {
+        id: ID
+        name: String
+        description: String
+        price: Float
+        soldout: Soldout
+        inventory: Int
+        stores: [Store]!
+    }
+
+    enum Soldout {
+        SOLDOUT
+        ONSALE
+    }
+
+    type Store {
         store: String
-        }
-        type Query {
-            getProduct(id: ID): Product
-        }
-        input StoreInput {
-          store: String
-        }
-        
-        input ProductInput {
-          id: ID
-          name: String
-          description: String
-          price: Float
-          soldout: Soldout
-          inventory: Int
-          stores: [StoreInput]!
-        }
-        
-        type Mutation {
-          createProduct(input: ProductInput) : Product
-        }
-  `)
+    }
 
+    type Query {
+        getProduct(id: ID): Product
+        getAllProducts: [Product]
+    }
 
-export default  schema
+    input StoreInput {
+        store: String
+    }
+
+    input ProductInput {
+        id: ID
+        name: String
+        description: String
+        price: Float
+        soldout: Soldout
+        inventory: Int
+        stores: [StoreInput]!
+    }
+
+    type Mutation {
+        createProduct(input: ProductInput): Product
+        updateProduct(input: ProductInput): Product
+        deleteProduct(id:ID!): String
+    }
+`)
+
+export default schema;
